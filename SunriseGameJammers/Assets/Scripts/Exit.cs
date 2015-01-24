0ebf4358b304c0d6fbbce8427967d68d86c32e59
Strakaman <straka3000@gmail.com>
2015-01-23 23:49:05 -0800

    work on timed press level

---
+++ SunriseGameJammers/Assets/Scripts/Exit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour
+{
+
+		public LevelManager lManager;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnTriggerEnter2D (Collision2D collInfo)
+		{
+				if (Utilities.hasMatchingTag (Tag.Player, collInfo.gameObject)) {
+						lManager.CompleteLevel ();
+						collInfo.gameObject.rigidbody2D.velocity = new Vector2(0,0);
+				}
+		}
+}
---
+++ SunriseGameJammers/Assets/Scripts/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public static class Utilities
+{
+
+	/*
+	 * Checks passed in Game object to see if any of it's tags are of the requested value
+	 */ 
+	public static bool hasMatchingTag(Tag tagToCheckFor, GameObject objectToCheck)
+	{
+		MultiTagScript mult =  objectToCheck.GetComponent<MultiTagScript>();
+		if (mult != null) { return mult.objectHasTag(tagToCheckFor);}
+		return false;
+	}
+
+}
+
