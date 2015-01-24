45af1efefe6fd88e7c5d62911ccc39017027d10e
Strakaman <straka3000@gmail.com>
2015-01-23 23:01:14 -0800

    Level Manager Code plus prefab

---
+++ SunriseGameJammers/Assets/LevelManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+		private bool levelComplete;
+		private bool levelFailed;
+		// Use this for initialization
+		void Start ()
+		{
+	
+				levelComplete = false;
+				levelFailed = true;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetButtonDown ("Action")) {
+						if (levelComplete) {		
+								Application.LoadLevel (Application.loadedLevel + 1);
+		
+						}
+						if (levelFailed) {
+								Application.LoadLevel (Application.loadedLevel);
+						}
+				}
+		}
+
+		public void CompleteLevel ()
+		{
+				levelComplete = true;
+		}
+
+		public void FailLevel ()
+		{
+				levelFailed = true;
+		}
+
+		void OnGUI ()
+		{
+				if (levelComplete) {
+						GUI.Label (new Rect (50, 50, 100, 100), "Level Complete!");
+				}
+				if (levelFailed) {
+						GUI.Label (new Rect (50, 50, 100, 100), "Level Failed...");
+				}
+		}
+
+		void OnLevelWasLoaded(int level)
+	{
+		Debug.Log ("Level " + Application.loadedLevel + " loaded");
+	}
+}
