cb170780c8907443d71721dd60c3d2579016aa23
Strakaman <straka3000@gmail.com>
2015-01-23 22:12:53 -0800

    ok

---
+++ SunriseGameJammers/Assets/Scripts/Hmm.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hmm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
