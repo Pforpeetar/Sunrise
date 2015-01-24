822d5ffa8306926b154c56bd267dcd555d379f78
Carl Lee <carlleeldex@gmail.com>
2015-01-23 22:47:13 -0800

    Mash Testing

---
+++ SunriseGameJammers/Assets/Scripts/Mash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mash : MonoBehaviour {
+
+	private int timesPressed = 0;
+	private float currentTime;
+	private float endTime;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = Time.time;
+		endTime = currentTime + 2.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		currentTime = Time.time;
+
+		if (Input.GetButtonDown ("Action") && currentTime < endTime) {
+			timesPressed++;
+			Debug.Log (timesPressed);
+		}
+	}
+}
