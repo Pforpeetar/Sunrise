fa8d23e36c61ebed4795ff8525e86156bbdc1659
Carl Lee <carlleeldex@gmail.com>
2015-01-24 19:39:51 -0800

    Added enemies to Mash level, missing sound

--- SunriseGameJammers/Assets/Scripts/Mash.cs
+++ SunriseGameJammers/Assets/Scripts/Mash.cs
@@ -1,15 +1,20 @@
-﻿using UnityEngine;
+﻿//Carl Lee
+//This player object will control most of what is going on in this game.
+//Cuz I'm lazy...
+
+using UnityEngine;
 using System.Collections;
 
 public class Mash : MonoBehaviour {
 
-	//This player object will control most of what is going on in this game.
 	public float mashDuration;		//How long can the player mash?
-	public float fadeDuration;		//How long will it take to fade to black
-	public int pressesToWin;		//How much presses needed to pass
+	public float fadeDuration;		//How long will it take to fade to black?
+	public int pressesToWin;		//How much presses needed to pass the level?
+	public float winForce;			//Force onto enemy when player wins (WHEEEEEEEEE!!!!)
 	public LevelManager lvlmanager;
 	public GameObject mashSlash;
 	public GameObject fader;
+	public GameObject enemies;
 
 	private int timesPressed = 0;	//Counter for # of times pressed
 	private float currentTime;
@@ -18,12 +23,13 @@ public class Mash : MonoBehaviour {
 	private float endLightFade;
 
 	//Setup Timer
-	//Spawn Enemies 
+	//Setup Fader
 	void Start () {
 		currentTime = Time.time;
 		endTime = currentTime + mashDuration + 0.5f + fadeDuration; //Difference from endLightFade and endTime is 3.0 seconds
 		startLightFade = currentTime + 0.5f;
 		endLightFade = currentTime + 0.5f + fadeDuration;
+
 		Color newA = new Color (fader.renderer.material.color.r, fader.renderer.material.color.g, fader.renderer.material.color.b, 0.0f);
 		fader.renderer.material.color = newA;
 	}
@@ -38,21 +44,28 @@ public class Mash : MonoBehaviour {
 		//Interpolation through time formula = A(1-t) + B(t)
 		//Alpha 0.0 = Transparent, Alpha 1.0 = Opaque, so A = 0.0 and B = 1.0
 		//We have t range from 0 to fadeDuration, but it needs to be scaled to a 0 to 1 range
-		//1/fadeDuration * currentTime - startLightFade
+		//1/fadeDuration * (currentTime - startLightFade)
 		if (currentTime > startLightFade && currentTime < endLightFade) {
 			float alpha = (1/fadeDuration) * (currentTime - startLightFade); //Never a negative number thankts to currentTime > startLightFade
 			Color newA = new Color (fader.renderer.material.color.r, fader.renderer.material.color.g, fader.renderer.material.color.b, alpha);
 			fader.renderer.material.color = newA;
 		}
 
-		//Allowed to mash when currentTime is within 3 seconds of the endTime
+		//Lights out, Allowed to mash when currentTime is within 3 seconds of the endTime
+		//Also stop enemy movement
 		bool mashCheck = false;
 		if (endTime - currentTime < mashDuration && endTime - currentTime > 0.0f) {
 			mashCheck = true;
-			//Last transparency check on fader
+			//Last transparency check on fader, make sure it is opaque
 			if (fader.renderer.material.color.a != 1.0f) {
 				Color newA = new Color (fader.renderer.material.color.r, fader.renderer.material.color.g, fader.renderer.material.color.b, 1.0f);
 				fader.renderer.material.color = newA;
+			}
+			//For each enemy, stop their momentum
+			//ATM, empty gameObject called enemies holds enemy1, enemy2, etc...
+			foreach (Transform enemy in enemies.gameObject.transform) {
+				enemy.gameObject.rigidbody2D.velocity = Vector2.zero;
+				enemy.gameObject.rigidbody2D.gravityScale = 0; //Disable gravity on them
 			}
 		}
 
@@ -62,17 +75,39 @@ public class Mash : MonoBehaviour {
 		if (Input.GetButtonDown ("Action") && mashCheck) {
 			timesPressed++;
 			//Spawn prefab
-			Vector3 newPos = new Vector3 (Random.Range (-2.0f, 2.0f), Random.Range (-3.0f, 1.0f), -2.0f);
+			Vector3 newPos = new Vector3 (Random.Range (-2.0f, 2.0f), Random.Range (-3.0f, -1.0f), -2.0f);
 			Quaternion newRot = Quaternion.Euler (0, 0, Random.Range (0.0f, 360.0f));
 			Instantiate(mashSlash, newPos, newRot);
+			//Counter Debug
 			Debug.Log (timesPressed);
 		}
 
 		//Akuma mode ended, lights back on
 		if (currentTime > endTime) {
+			//When lights go back on, give the enemies momentum as well, depends on win or lose
 			if (fader.renderer.material.color.a != 0.0f) {
 				Color newA = new Color (fader.renderer.material.color.r, fader.renderer.material.color.g, fader.renderer.material.color.b, 0.0f);
 				fader.renderer.material.color = newA;
+
+				//Start moving enemies again (Control it here, I'm dumb and I don't know how to call other object's functions)
+				foreach (Transform enemy in enemies.gameObject.transform) {
+					enemy.gameObject.rigidbody2D.gravityScale = 1; //Re-enable gravity (I dont think this works anymore >_>)
+					//Enemies will fly toward the player on lose, away from player on win.
+					if (timesPressed >= pressesToWin) { //Win
+						//Get vector facing away from the player current position
+						//enemyPosition - playerPosition
+						Vector2 newDir = enemy.gameObject.transform.position - transform.position;
+						newDir.Normalize ();
+						enemy.gameObject.rigidbody2D.AddForce(newDir * winForce);
+					}
+					else { //Lose
+						//Get vector facing towards the player current position
+						//playerPosition - enemyPosition
+						Vector2 newDir = transform.position - enemy.gameObject.transform.position;
+						newDir.Normalize ();
+						enemy.gameObject.rigidbody2D.AddForce(newDir * winForce);
+					}
+				}
 			}
 		}
 
