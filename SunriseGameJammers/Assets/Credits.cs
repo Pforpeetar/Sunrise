2ea81831fed894e70d497af5e6ccf72d1f785afc
Strakaman <straka3000@gmail.com>
2015-01-25 01:01:37 -0800

    credits scene

--- SunriseGameJammers/Assets/Credits.cs
+++ SunriseGameJammers/Assets/Credits.cs
@@ -16,24 +16,10 @@ public class Credits : MonoBehaviour {
 	void Start () {
 		credits = new Dictionary<string, string[]>()
 		{
-			{ "Blank Design Team" , new string[]{"Peter Pham", "Omari Straker", "Cecilia Tran", "Jacob Day", "Wilson Loi", "Dennis Hsu"}},
-			{ "Director" , new string[]{"Peter Pham"}},
-			{ "Lead Programmer" , new string[]{"Omari Straker"}},
-			{ "Art Assets" , new string[]{"Cecilia Tran","Peter Pham"}},
-			{ "Menus" , new string[]{"Jacob Day", "Omari Straker"}},
-			{ "Level Design" , new string[]{"Peter Pham","Omari Straker"}},
-			{ "Sound Effects" , new string[]{
-					"Switch Click - www.kenney.nl/", "NPC Voices - www.text2speech.org/", "",
-					"Freesound.org: User Uploaded Sounds","Pause Noise - Northern_Monkey",
-					"Fire Spell - spookymodem","Water Spell - cusconauta","Yellow Spell - futureprobe",
-					"Charged Yellow Spell - parnellij","Player Charging - javierzumer",
-					"Item Pickup - crashoverride61088","","Other Sounds - Omari Straker"}},
-			{ "Background Music" , new string[]{
-					"Light Years - www.soundimage.org/sci-fi/", "Dark Techno City - www.soundimage.org/sci-fi/",
-					"World of Automatons - www.soundimage.org/sci-fi/","Dystopic Factory - www.soundimage.org/sci-fi/",
-					"Dystopic Technology - www.soundimage.org/sci-fi/", "Uncertain Future - www.soundimage.org/sci-fi/",
-					"Credits Theme - Jacob Day"}},
-			{ "Additional Thanks" , new string[]{"SJSU Game Dev Club","Megaman X4 Sprites"}},
+			{ "Sunrise Design Team" , new string[]{"Peter Pham", "Omari Straker", "Carl Lee", "Travis Tran"}},
+			{ "Programmers" , new string[]{"Carl Lee", "Peter Pham", "Omari Straker"}},
+			{ "Art Assets" , new string[]{"Travis Tran"}},
+			{ "Additional Thanks" , new string[]{"SJSU Game Dev Club","Global Game Jam 2015"}},
 		};
 		keys = new List<string>(credits.Keys); //store keys in a seperate array for easier reference
 		index = 0; //use index to keep track of which part of credits we are displaying
@@ -43,7 +29,7 @@ public class Credits : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("pause"))
+		if (Input.GetButtonDown ("Action"))
 		{
 			GoToMainMenu();
 		}
@@ -64,7 +50,7 @@ public class Credits : MonoBehaviour {
 		for (int i = 0; i < currNames.Length; i++) {
 			GUI.Label(new Rect (Screen.width/2-50,100+i*25,100,50),currNames[i],creditWordStyle);
 		}
-		GUI.Label(new Rect (Screen.width-215,Screen.height - 65,100,50),"Press Pause (P) for Main Menu",creditWord2);
+		GUI.Label(new Rect (3*Screen.width/4,Screen.height - 65,100,50),"Press Action (spacebar) for Main Menu",creditWord2);
 	}
 	/**
 	 * Since we draw the credit info based on the value of the index counter,
@@ -76,7 +62,7 @@ public class Credits : MonoBehaviour {
 		while(index < keys.Count) {
 			currKey = keys[index];
 			currNames = credits[currKey];
-			yield return new WaitForSeconds(1.0f + currNames.Length*.65f);
+			yield return new WaitForSeconds(1.0f + currNames.Length*.85f);
 			//yield return new WaitForSeconds(1.0f);
 			index++;
 		}
@@ -86,6 +72,6 @@ public class Credits : MonoBehaviour {
 	
 	void GoToMainMenu()
 	{
-		Application.LoadLevel("Main Menu");
+		Application.LoadLevel(0);
 	}
 }
