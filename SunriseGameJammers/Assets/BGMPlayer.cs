2a879684aa5371c3deeaa4d082f9d038f27b9fae
Strakaman <straka3000@gmail.com>
2015-01-25 11:54:38 -0800

    small fixes and a music player

---
+++ SunriseGameJammers/Assets/BGMPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BGMPlayer : MonoBehaviour {
+	public static BGMPlayer soundManager;
+	public AudioClip mainTheme;
+	public AudioClip stageMusic;
+	private List<string> mainThemeStages = new List<string>(){"Main Menu","Credits"};
+	// Use this for initialization
+	void Awake()
+	{
+		if (!soundManager) {
+			soundManager = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start () {
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		if (audio == null) {return;}
+		AudioClip newClip;
+		if(mainThemeStages.Contains(Application.loadedLevelName))
+		{
+			newClip = mainTheme;
+		}
+		else
+		{
+			newClip = stageMusic;
+		}
+		if (audio.clip != newClip)
+		{
+			audio.clip = newClip;
+			audio.Play();
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
